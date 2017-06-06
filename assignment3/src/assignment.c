b10415b5f6930e87d59c9aa2ada7cc65f9642e12
Aleksandr Epp <aleksandr.epp@gmail.com>
2017-06-06 21:56:36 +0200

    Add parameters to description

--- assignment3/src/assignment.c
+++ assignment3/src/assignment.c
@@ -3,7 +3,7 @@
 /* Author: Aleksandr Epp <aleksandr.epp@gmail.com>                      */
 /* Matriclenumber: 6002853                                              */
 /* Assignment : 3                                                       */
-/* Parameters: -t, -e                                                   */
+/* Parameters: -h, -t, -m, -v, -s, -e       see help (-h) for details   */
 /* Environment variables: no                                            */
 /*                                                                      */
 /* Description:                                                         */
@@ -11,35 +11,41 @@
 /* This is the main program of Assignment 3. From here individual tasks */
 /* can be started. Or view the help.                                    */
 /*                                                                      */
-/************************************************************************/ 
+/************************************************************************/
 
-#include <stdio.h>          // import of the definitions of the C IO library
-#include <stdlib.h>         // to use exit() function
-#include <string.h>         // import of the definitions of the string operations
-#include <unistd.h>         // standard unix io library definitions and declarations
-#include <errno.h>          // system error numbers
-#include <inttypes.h>       // for strtoumax() string to int conversion
+#include <stdio.h>                      // import of the definitions of the C IO library
+#include <stdlib.h>                     // to use exit() function
+#include <string.h>                     // import of the definitions of the string operations
+#include <unistd.h>                     // standard unix io library definitions and declarations
+#include <errno.h>                      // system error numbers
+#include <inttypes.h>                   // for strtoumax() string to int conversion
 
-#include "help.h"           // to show help with -h parameter
-#include "task1.h"          // to run Task 1
-#include "task2.h"          // to run Task 2
+#include "help.h"                       // to show help with -h parameter
+#include "task1.h"                      // to run Task 1
+#include "task2.h"                      // to run Task 2
+
+#include <linux/limits.h>   // for max path name length
 
 // constants & default values
 #define ASSIGNMENT_NR   3
-#define EPSILON         0.0000001     // default epsilon value for Jacobi method
-#define MATRIX          ((unsigned char *)"./examples/Matrix_A_8x8")    // default matrix A path
-#define VECTOR_B        ((unsigned char *)"./examples/Vector_b_8x")     // default vector b path
-#define VECTOR_B_SIZE   8
+#define EPSILON         0.0000001       // default epsilon value for Jacobi method
+#define VECTOR_B_SIZE   8               // default vector b size
 
 int main(int argc, char* argv[])
-{ 
-    int opt = 0,                          // command line parameter name
-        vectorBSize = VECTOR_B_SIZE;      // size of vector b
-    double epsilon = EPSILON;             // epsilon value for Jacobi method
+{
+    int opt = 0,                        // command line parameter name
+        vectorBSize = VECTOR_B_SIZE;    // size of vector b
+    double epsilon = EPSILON;           // epsilon value for Jacobi method
 
-    char *taskName = NULL,                // task nr. given by cli parameter
-         *matrixFilePath = MATRIX,        // path to the file containing matrix A
-         *vectorBFilePath = VECTOR_B;     // path to the file containing vector b
+    char *taskName = NULL,                                  // task nr. given by cli parameter
+         matrixAFilePath[PATH_MAX],      // default path to the file containing matrix A
+         vectorBFilePath[PATH_MAX];     // default path to the file containing vector b
+
+    memset(matrixAFilePath, 0, PATH_MAX);
+    memset(vectorBFilePath, 0, PATH_MAX);
+
+    strcpy(matrixAFilePath, "./examples/Matrix_A_8x8");
+    strcpy(vectorBFilePath, "./examples/Vector_b_8x");
 
     // iterate through all cli parameters
     while ((opt = getopt(argc, argv, "t:e:m:v:s:h")) != -1) {
@@ -51,10 +57,12 @@ int main(int argc, char* argv[])
                 taskName = optarg;
                 break;
             case 'm': // if -m parameter given => set path to matrix file
-                matrixFilePath = optarg;
+//                matrixAFilePath = optarg;
+                strcpy(matrixAFilePath, optarg);
                 break;
             case 'v': // if -v parameter given => set path to vector b file
-                vectorBFilePath = optarg;
+//                vectorBFilePath = optarg;
+                strcpy(vectorBFilePath, optarg);
                 break;
             case 's': // if -s parameter given => set the size of vector b
                 vectorBSize = strtoumax(optarg, NULL, 10);
@@ -73,9 +81,9 @@ int main(int argc, char* argv[])
         showHelp(ASSIGNMENT_NR, EPSILON, argc, argv);
         exit(0);
     } else if(!strcmp(taskName, "1")) {
-        task1(argc, argv, epsilon, matrixFilePath, vectorBFilePath, vectorBSize);
+        task1(argc, argv, epsilon, matrixAFilePath, vectorBFilePath, vectorBSize);
     } else if(!strcmp(taskName, "2")) {
-        task2(argc, argv, epsilon, matrixFilePath, vectorBFilePath, vectorBSize);
+        task2(argc, argv, epsilon, matrixAFilePath, vectorBFilePath, vectorBSize);
     } else { // if invalid task number provided
         printf("Task %s doesn't exist. See help (-h) for available tasks.\n", taskName);
     }
